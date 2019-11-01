34f0d4acbace61eea70b37e0378a57ed2db25847
Saqib Nazir <saqib_9090@yahoo.com>
2019-11-01 04:52:38 +0100

    some css

--- src/Nav.js
+++ src/Nav.js
@@ -4,9 +4,13 @@ import { MovieContext } from './MovieContext';
 const Nav = () => {
   const [movies, setMovies] = useContext(MovieContext);
   return (
-    <div>
-      <h3>Saqib</h3>
-      <p>List of Movies:{movies.length}</p>
+    <div className="App-header navbar">
+      <ul>
+        <li> Movie DATA </li>
+        <li>List of Movies:{movies.length}</li>
+      </ul>
+      {/* <h3>Saqib</h3>
+      <p>List of Movies:{movies.length}</p> */}
     </div>
   );
 };
